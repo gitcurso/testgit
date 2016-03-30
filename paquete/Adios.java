367a96a695007414fd078010d2ad7e2dafc47cd2
Zeus Gomez <zeus@stucom.com>
2016-03-30 20:22:16 +0200

    Es 25 tio

--- paquete/Adios.java
+++ paquete/Adios.java
@@ -1,6 +1,6 @@
 public class Adios {
   static String nombre = "UN TIO";
-	static int edad = 23; 
+	static int edad = 25; 
   // ENTRA TODO AQUI
   public static void main(String[] args) {
     System.out.println("HOLA " + nombre + edad);
