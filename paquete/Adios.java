591e06598da66584b0a48a72e25bde0fc4fd79d1
Zeus Gomez <zeus@stucom.com>
2016-03-30 20:09:39 +0200

    A TOPE

--- paquete/Adios.java
+++ paquete/Adios.java
@@ -1,8 +1,8 @@
-public class Hola {
+public class Adios {
   static String nombre = "UN TIO";
-
+	static int edad = 23; 
   // ENTRA TODO AQUI
   public static void main(String[] args) {
-    System.out.println("HOLA " + nombre);
+    System.out.println("HOLA " + nombre + edad);
   }
 }
